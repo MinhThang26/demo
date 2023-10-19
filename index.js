2bb2d989731968d651b4325fbc18d57078c877b3
MinhThang26 <86099702+MinhThang26@users.noreply.github.com>
2023-10-19 10:40:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+
 var bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const app = express();
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cors());
+app.use(morgan("common"));
 
 app.get("/api", (req, res) => {
   res.status(200).json("hello");
